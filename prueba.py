823ccff64acec10eade8251249be83ff9b00eb04
game00000 <95388678+game00000@users.noreply.github.com>
2021-12-01 20:02:30 -0600

    Create prueba.py

---
+++ prueba.py
@@ -0,0 +1,193 @@
+#############librerias1#############
+import os
+import sys                    
+####################################
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias |")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias /")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias -")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias \")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias |")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias /")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias -")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias \")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias |")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias /")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias -")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias \")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias |")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias /")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias -")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias \")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias |")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias /")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias -")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias \")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias |")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias /")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias -")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias \")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias |")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias /")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias -")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias \")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias |")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias /")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias -")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias \")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias |")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias /")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias -")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias \")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias |")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias /")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias -")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias \")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias |")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias /")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias -")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "necesitas estas librerias \")
+os.system("clear")
+print(v + "[" + n + "!" + v + "]" + b + "instalando...")
+
+#instalacion de librerias necesarias
+os.system("pip install time")
+os.system("pip install pyshark")
+os.system("pip install python-nmap")
+os.system("pip install wikipedia")
+print("bien listo")
+#########librerias2#########
+import time
+import pyshark
+import nmap
+import wikipedia as wp
+############################
+#####variables#de#color#####
+b = '\033[0m'  # blanco
+R = '\033[31m'  # rojo
+v = '\033[32m'  # verde
+n = '\033[33m'  # naranja
+a = '\033[34m'  # azul
+m = '\033[35m'  # morado
+c = '\033[36m'  # cyan
+g = '\033[37m'  # gris
+############################
+print(v + """
+
+     _   _     ___      _____  _  __
+    | |_| |   / _ \    / ___/ | |/ /  
+    |  _  |  / /_\ \  | |___  | |\ \
+    |_| |_| / /‾‾‾\ \  \____\ |_| \_\.py
+    
+                 >>> beta 1.0 <<<
+             >>>echo por game00000<<<            
+""")
+print("===================|menu|======================")
+print("[1] escaner de puertos                          ")
+print("[2] sniffer")
+print("[3] buscar en wikipedia")
+print("[4] cracker(no disponible)")
+print("[5] wifi scanner(no disponible)")
+print("[6] wifi cracker")
+print("[0] salir
+print("===============================================")
+menu = input(b + "[" + v + "+" + b + "]" + "seleccione :")
+if menu == 1 :
+   ip = input("[?] Introdusca la ip o sitio web a scanear")
+   nm=nmap.PortScanner()
+   resulta=nmap.scan(ip)
+   puertoabiertos = "-p "
+   #print(resulta)
+   #######resultado###########
+   print('==============================================')
+   print('Host : %s (%s)' % (host, nm[host].hostname()))
+   print('State : %s' % nm[host].state())
+   for proto in nm[host].all_protocols():
+       print('================|scanner|====================')
+       print('Protocolo : %s' % proto)
+
+       lport = nm[host][proto].keys()
+       lport.sort()
+       for port in lport:
+	         print ("\033[31m[\033[32m+\033[31m]" + '\033[0m puerto : %s\tEstado : %s' % (port, nm[host][proto][port]['state']))
+           print("[+]estos son los puertos espero a verte ayudado :) !")
+           exit()
+if menu == 2 :   
+   select = input("[?]seleccione la interfaz: ")
+   capture = pyshark.LiveCapture(interface=select)
+   capture.sniff(timeout=50)
+   capture
+
+   capture[3]
+
+
+   for packet in capture.sniff_continuously(packet_count=5):
+       print 'Just arrived:', packet
+       print("[+]el Scan termino...")
+       exit()
+if menu == 3 :
+   cagada = input("[?]seleccione lo que decea buscar: ")
+   print("--------|resultados de la busqueda|---------")
+   print("")
+   resp=wp.search(cagada)
+   print("==========|especificaciones y etc|==========")
+   print("")
+   espe=wp.sumary(cagada)
+   print("============================================")
+   input("[!]presione para continuar")
+   input("[!]SEGURO QUE DECEA SALIR?")
+   exit()
+if menu == 6 :
+   print("[!]necesitas instalar aircrack-ng")
+   time.sleep(3)
+   os.system("clear")
+   print(b + "[" + a + "+" + b + "]instalando aircrack-ng...")
+   time.sleep(3)
+   os.system("apt-get install aircrack-ng")
+   print("listo diviertete >:)")
+   time.sleep(5)
+   exit()
+   
+   
